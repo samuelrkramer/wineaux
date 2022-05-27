56f4590aa3cfc91744230552f2ef504cb392c630
Sam Kramer <SamuelRKramer@gmail.com>
2022-05-27 13:30:18 -0400

    fix typo

--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -54,5 +54,5 @@ def seed_reviews():
 # resets the auto incrementing primary key, CASCADE deletes any
 # dependent entities
 def undo_reviews():
-    db.session.execute('TRUNCATE rewviews RESTART IDENTITY CASCADE;')
+    db.session.execute('TRUNCATE reviews RESTART IDENTITY CASCADE;')
     db.session.commit()
