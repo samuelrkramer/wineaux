ed8586291b0d79151a40cf3671cc64d459fc8835
Sam Kramer <SamuelRKramer@gmail.com>
2022-05-31 19:35:44 -0400

    wine form with hard-coded colors but we're going to hit the databse on the form component for varieties

---
+++ app/forms/wine_form.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SelectField, TextField
+from wtforms.validators import DataRequired #, Email, ValidationError
+# from app.models import Wine
+
+class WineForm(FlaskForm):
+    # wine_id = IntegerField('wine_id', validators=[DataRequired()])
+    name = StringField('name', validators=[DataRequired()])
+    year = IntegerField('year')
+    variety_id = SelectField('variety_id', validators=[DataRequired()])
+    description = TextAreaField('description')
+    color = SelectField('color', validators=[DataRequired()],
+        choices=[
+            ('Gray'),
+            ('Orange'),
+            ('Red'),
+            ('Rosé'),
+            ('Tawny'),
+            ('White'),
+            ('Yellow'),
+        ])
+    sweetness = StringField('sweetness', validators=[DataRequired()])
+    user_id = IntegerField('user_id', validators=[DataRequired()])
+    image_url = StringField('image_url')
