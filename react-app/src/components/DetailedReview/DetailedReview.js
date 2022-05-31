9dca0a9447d04dd3d5071fd62b16a44648212f08
da5idf <davidpforster24@gmail.com>
2022-05-31 15:10:45 -0400

    edit text of review

--- react-app/src/components/DetailedReview/DetailedReview.js
+++ react-app/src/components/DetailedReview/DetailedReview.js
@@ -1,23 +1,51 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 import './DetailedReview.css';
-import { getOneReview } from '../../store/reviews'
+import { getOneReview } from '../../store/reviews';
+import EditReviewText from './EditReviewText';
+import ratingEmpty from '../../images/ratingEmpty.png';
+import ratingFull from '../../images/ratingFull.png';
+
+const displayRating = rating => {
+    let reviews = [];
+
+    for (let i = 1; i <= 5; i++) {
+        if (i <= rating) {
+            reviews.push((<img src={ratingFull} alt="" key={i} />))
+        } else {
+            reviews.push((<img src={ratingEmpty} alt="" key={i} />))
+        }
+    }
+    return reviews;
+}
 
 function DetailedReview() {
+
     const dispatch = useDispatch();
 
     const { reviewId } = useParams();
 
     const review = useSelector(state => state.reviews.singleReview);
+    const user = useSelector(state => state.session.user)
+
+    const [inEdit, setInEdit] = useState(false);
 
     useEffect(() => {
         dispatch(getOneReview(reviewId));
     }, [dispatch, reviewId])
 
+
     if (!review.id) {
         return <h1>Loading</h1>
+    }
+
+    const edit = review.user.id === user.id ? true : false;
+
+    const textEdit = (e) => {
+        if (!edit) return
+        setInEdit(true);
     }
 
     return (
@@ -39,13 +67,18 @@ function DetailedReview() {
                             <div id="dr-wine-year">{review.wine.year}</div>
                         </div>
                     </div>
-                    <div id="dr-wine-rating">{review.wine.rating}</div>
+                    <div id="dr-rating-container">
+                        {displayRating(review.rating)}
+                    </div>
                     <div id="dr-review-date">{review.updatedAt}</div>
+                    {inEdit ?
+                        <EditReviewText review={review} setInEdit={setInEdit} /> :
+                        <div id={`dr-review-text-${edit}`} onClick={textEdit}>{review.text}</div>
+                    }
                 </div>
             </div>
-            <div id="dr-img-container">
-                <img id="dr-img" src={review.image_url} alt="" />
-            </div>
+
+            <img id="dr-img" src={review.image_url} alt="" />
 
         </div>
     )
