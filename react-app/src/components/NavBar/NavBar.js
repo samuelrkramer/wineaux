c295df853e84d18a8264d58a424b6df16606f236
Jonathon Tufts <86862338+jonathontufts@users.noreply.github.com>
2022-06-03 11:57:02 -0400

    cleanup logs & styling

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -2,7 +2,6 @@
 import React, { useState, useEffect} from "react";
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import LogoutButton from '../auth/LogoutButton';
 import ProfileButton from './ProfileButton';
 import Logo from './Logo';
 
@@ -18,8 +17,6 @@ const NavBar = () => {
   useEffect(() => {
     setUser(sessionUser)
   }, [user, sessionUser])
-
-  console.log(sessionUser)
 
   return (
     <nav id='nav_bar'>
@@ -55,8 +52,8 @@ const NavBar = () => {
           {
             user && (
               <div className='nav_link_div'>
-                <NavLink to='/users' exact={true} activeClassName='active' className='nav_link'>
-                  Users
+                <NavLink to='/wines/new' exact={true} activeClassName='active' className='nav_link'>
+                  Upload Wine
                 </NavLink>
               </div>
             )
